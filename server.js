2f49bca760f37f60159c206707f9b277f7565981
sergio <sergio.cb87@gmail.com>
2025-01-30 23:56:36 -0300

    Corrigiendo duplicación de serviceAccount

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const admin = require('firebase-admin');
-const serviceAccount = require("./serviceAccountKey.json");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
