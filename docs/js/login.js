0c187e2305a41906bfb9385d4ee4cbb372ad7475
chech00 <110186409+chech00@users.noreply.github.com>
2025-01-31 03:24:52 -0300

    Update login.js

--- docs/js/login.js
+++ docs/js/login.js
@@ -8,6 +8,7 @@ const firebaseConfig = {
   appId: "1:267782898691:web:751f881080a7debd67fa36"
 };
 
+
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
